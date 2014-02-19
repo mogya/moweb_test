cdc40fefc44bade7e1bba145d433c938b910d95e
mogya <mogya@mogya.com>
2014-02-19 21:22:34 +0900

    駅検索API、都道府県別駅名表示機能のテスト

--- api/v0_station.rb
+++ api/v0_station.rb
@@ -11,12 +11,12 @@ class V0_station < TestCaseForAPI
     assert_match /ゆぜおんせん/, station[:kana], "kana match"
     assert_match /JR花輪線/, station[:line], "line match"
     assert_match /秋田県鹿角市/, station[:area], "area match"
-    assert station[:lat].to_f<50 , "lat check"
-    assert station[:lat].to_f>30 , "lat check"
-    assert station[:lng].to_f<150 , "lng check"
-    assert station[:lng].to_f>120 , "lng check"
+    assert station[:latitude].to_f<50 , "lat check"
+    assert station[:latitude].to_f>30 , "lat check"
+    assert station[:longitude].to_f<150 , "lng check"
+    assert station[:longitude].to_f>120 , "lng check"
   end
-  def test_station2
+  def test_station_area
     params = {area:'秋田県'}
     json = getJSON( "#{@base_url}v0/station?#{ URI.encode_www_form(params) }" )
     assert_match 'OK', json[:status], "status check"
@@ -27,9 +27,43 @@ class V0_station < TestCaseForAPI
     assert_match /ゆぜおんせん/, station[:kana], "kana match"
     assert_match /JR花輪線/, station[:line], "line match"
     assert_match /秋田県鹿角市/, station[:area], "area match"
-    assert station[:lat].to_f<50 , "lat check"
-    assert station[:lat].to_f>30 , "lat check"
-    assert station[:lng].to_f<150 , "lng check"
-    assert station[:lng].to_f>120 , "lng check"
+    assert station[:latitude].to_f<50 , "lat check"
+    assert station[:latitude].to_f>30 , "lat check"
+    assert station[:longitude].to_f<150 , "lng check"
+    assert station[:longitude].to_f>120 , "lng check"
+  end
+  def test_station_name
+    params = {name:'湯瀬温泉'}
+    json = getJSON( "#{@base_url}v0/station?#{ URI.encode_www_form(params) }" )
+    assert_match 'OK', json[:status], "status check"
+    station = json[:results][0]
+    assert_match /湯瀬温泉/, station[:name], "name match"
+    assert_match /ゆぜおんせん/, station[:kana], "kana match"
+    assert_match /JR花輪線/, station[:line], "line match"
+    assert_match /秋田県鹿角市/, station[:area], "area match"
+    assert station[:latitude].to_f<50 , "lat check"
+    assert station[:latitude].to_f>30 , "lat check"
+    assert station[:longitude].to_f<150 , "lng check"
+    assert station[:longitude].to_f>120 , "lng check"
+  end
+  def test_station_order
+    params = {name:'福島',order:'latitude'}
+    json = getJSON( "#{@base_url}v0/station?#{ URI.encode_www_form(params) }" )
+    assert_match 'OK', json[:status], "status check"
+    _lat = 0.0
+    json[:results].each{|station|
+        assert_compare station[:latitude].to_f, '>=' , _lat, 'station.lat should be sorted.'
+        _lat = station[:latitude].to_f
+    }
+
+    params = {name:'福島',order:'longitude'}
+    json = getJSON( "#{@base_url}v0/station?#{ URI.encode_www_form(params) }" )
+    assert_match 'OK', json[:status], "status check"
+    _lng = 0.0
+    json[:results].each{|station|
+        assert(station[:longitude].to_f>=_lat, 'station.lng should be sorted.')
+        _lat = station[:longitude].to_f
+    }
+
   end
 end
---
+++ pc/area.rb
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+$:.unshift File.dirname(__FILE__)+'/../'
+require "saucelabs.rb"
+
+class Area < SaucelabsTestCasePC
+  def test_area
+    @browser.get(@base_url + "/area/")
+    assert element_present?(:link, "京都府")
+    @browser.find_element(:link, "京都府").click
+    assert element_present?(:css, 'input[value="三条駅"]')
+    @browser.find_element(:css, 'input[value="三条駅"]').click
+    assert_match /三条駅/, @browser.title
+  end
+  def test_area_tokyo
+    @browser.get(@base_url + "/area/")
+    assert element_present?(:link, "東京都")
+    @browser.find_element(:link, "東京都").click
+    assert element_present?(:link, "港区")
+    @browser.find_element(:link, "港区").click
+    assert element_present?(:css,'input[value="品川駅"]')
+    @browser.find_element(:css,'input[value="品川駅"]').click
+    assert_match /品川駅/, @browser.title
+  end
+
+end
